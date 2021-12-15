276df4ec3089170d82a6cf8caa68d82a17bdeac8
elianiva <dicha.arkana03@gmail.com>
2021-12-15 19:19:11 +0700

    feat(ui): migrate to ChakraUI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,15 +5,17 @@ import App from "./App";
 import { createStore } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ChakraProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
