78c54e1df5cc2cb5aea0e1f21d1b0df39320dbd0
elianiva <dicha.arkana03@gmail.com>
2021-12-15 21:18:11 +0700

    feat(typography): use inter and noto-sans

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,12 +6,22 @@ import { createStore } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
 import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import "@fontsource/inter";
+import "@fontsource/noto-sans";
 
 const store = createStore(rootReducer);
 
+const theme = extendTheme({
+  fonts: {
+    heading: "Inter",
+    body: "Inter"
+  }
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Provider store={store}>
         <App />
       </Provider>
