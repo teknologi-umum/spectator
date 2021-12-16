fc41020ba8464deea4bf04a6eeed3afce85b682e
elianiva <dicha.arkana03@gmail.com>
2021-12-16 11:41:39 +0700

    feat(samtest): make the form wider

--- frontend/src/pages/SAMTest/SAMTest.tsx
+++ frontend/src/pages/SAMTest/SAMTest.tsx
@@ -69,80 +69,73 @@ export default function SAMTest({ nextQuestion }: SAMTestProps) {
   }
 
   return (
-    <Flex
-      w="full"
-      h="full"
-      alignItems="center"
-      justifyContent="center"
-      px="4"
-      bg="gray.100"
+    <Box
+      as="form"
+      onSubmit={handleSubmit}
+      mt="20"
+      p="6"
+      rounded="md"
+      shadow="lg"
+      maxW="1300"
+      mx="auto"
+      bg="white"
     >
-      <Box
-        as="form"
-        onSubmit={handleSubmit}
-        mt="8"
-        p="6"
-        rounded="md"
-        shadow="lg"
-        maxW="container.md"
-        bg="white"
-      >
-        <Heading size="lg" color="gray.700" textAlign="center" mb="8">
-          Self Assessment Manikin Test (SAM Test)
-        </Heading>
+      <Heading size="lg" color="gray.700" textAlign="center" mb="8">
+        Self Assessment Manikin Test (SAM Test)
+      </Heading>
 
-        {currentPage === 0 && (
-          <Fade in={currentPage === 0}>
-            <Box>
-              <Text fontWeight="bold" fontSize="xl" mb="2">
-                How aroused are you now?
-              </Text>
-              <Text fontSize="lg" mb="4">
-                Arousal refer to how aroused are you generally in the meantime
-              </Text>
-              {getResponseOptions("arousal", arousal, setArousal)}
-            </Box>
-          </Fade>
-        )}
+      {currentPage === 0 && (
+        <Fade in={currentPage === 0}>
+          <Box>
+            <Text fontWeight="bold" fontSize="xl" mb="2">
+              How aroused are you now?
+            </Text>
+            <Text fontSize="lg" mb="4">
+              Arousal refer to how aroused are you generally in the meantime
+            </Text>
+            {getResponseOptions("arousal", arousal, setArousal)}
+          </Box>
+        </Fade>
+      )}
 
-        {currentPage === 1 && (
-          <Fade in={currentPage === 1}>
-            <Box>
-              <Text fontWeight="bold" fontSize="xl">
-                How pleased are you now?
-              </Text>
-              <Text fontSize="lg">
-                Pleasure refer to how pleased are you generally in the meantime
-              </Text>
-              {getResponseOptions("pleasure", pleasure, setPleasure)}
-            </Box>
-          </Fade>
-        )}
+      {currentPage === 1 && (
+        <Fade in={currentPage === 1}>
+          <Box>
+            <Text fontWeight="bold" fontSize="xl" mb="2">
+              How pleased are you now?
+            </Text>
+            <Text fontSize="lg">
+              Pleasure refer to how pleased are you generally in the meantime
+            </Text>
+            {getResponseOptions("pleasure", pleasure, setPleasure)}
+          </Box>
+        </Fade>
+      )}
 
-        <Flex justifyContent="space-between" mt="4">
-          <Button
-            colorScheme="blue"
-            variant="outline"
-            onClick={() => goto("prev")}
-            disabled={currentPage === 0}
-          >
-            Previous
-          </Button>
-          {currentPage === 1 ? (
+      <Flex justifyContent="end" mt="4" gap="4">
+        {currentPage === 1 ? (
+          <>
+            <Button
+              colorScheme="blue"
+              variant="outline"
+              onClick={() => goto("prev")}
+            >
+              Previous
+            </Button>
             <Button colorScheme="blue" variant="solid">
               Finish
             </Button>
-          ) : (
-            <Button
-              colorScheme="blue"
-              variant="solid"
-              onClick={() => goto("next")}
-            >
-              Next
-            </Button>
-          )}
-        </Flex>
-      </Box>
-    </Flex>
+          </>
+        ) : (
+          <Button
+            colorScheme="blue"
+            variant="solid"
+            onClick={() => goto("next")}
+          >
+            Next
+          </Button>
+        )}
+      </Flex>
+    </Box>
   );
 }
