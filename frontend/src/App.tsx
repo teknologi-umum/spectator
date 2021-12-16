d30db45e8efb051fb3a26a187ea9d02b6c2a597b
elianiva <dicha.arkana03@gmail.com>
2021-12-16 12:00:28 +0700

    refactor: start fresh

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,40 +1,45 @@
-import React, { Component } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LastPage from "./components/layout/lastPage";
-import PersonalInfo from "./components/basicInfo/PersonalInfo";
-import Instructions from "./components/instructions/instructions";
-import SAMTest from "./components/samtest/samtest";
-import CountDownTimer from "./components/coding/CountDownTimer";
-import Q1 from "./components/coding/Q1";
-// import Q2 from './components/coding/Q2';
-// import Q3 from './components/coding/Q3';
-// import Q4 from './components/coding/Q4';
-// import Q5 from './components/coding/Q5';
-// import Q6 from './components/coding/Q6';
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-class App extends Component {
-  render() {
-    const hoursMinSecs = { hours: 1, minutes: 30, seconds: 0 };
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <CountDownTimer hoursMinSecs={hoursMinSecs} />
-          <Routes>
-            <Route path="/" element={<PersonalInfo />} />
-            <Route path="/last" element={<LastPage />} />
-            <Route path="/instructions" element={<Instructions />} />
-            <Route path="/samtest" element={<SAMTest />} />
-            <Route path="/Q1" element={<Q1 />} />
-            {/* <Route path='/Q2' component={Q2} /> */}
-            {/* <Route path='/Q3' component={Q3} /> */}
-            {/* <Route path='/Q4' component={Q4} /> */}
-            {/* <Route path='/Q5' component={Q5} /> */}
-            {/* <Route path='/Q6' component={Q6} /> */}
-          </Routes>
-        </div>
-      </BrowserRouter>
-    );
-  }
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { createStore } from "redux";
-import rootReducer from "./store/reducers/rootReducer";
-import { Provider } from "react-redux";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-const store = createStore(rootReducer);
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
