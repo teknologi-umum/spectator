7058490f8efebf2cd469defbf57aba37d96874fd
elianiva <dicha.arkana03@gmail.com>
2021-12-18 10:42:36 +0700

    refactor: use React.FC instead

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,10 +1,12 @@
 import { Box } from "@chakra-ui/react";
-import { ReactChild } from "react";
 
-export default function Layout({ children }: { children: ReactChild }) {
+// eslint-disable-next-line react/prop-types
+const Layout: React.FC = ({ children }) => {
   return (
     <Box bg="gray.100" alignItems="center" w="full" minH="full" py="10" px="4">
       {children}
     </Box>
   );
-}
+};
+
+export default Layout;
