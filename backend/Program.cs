035d1d21d1723c8cea6e7462b6a8baed8c1669dc
vmasdani <valianmasdani@gmail.com>
2021-12-19 21:50:17 +0700

    add sam test and user information endpoint

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,11 @@
+using System;
+using System.Linq;
+using System.Text.Json;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,11 +18,29 @@ builder.Services.AddSwaggerGen();
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+if (app.Environment.IsDevelopment()) {
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
+
+// SAM test data
+app.MapGet("/sam-test", () => {
+	return "SAM test endpoint";
+});
+
+// Get JWT through authorization header, parse it
+app.MapGet("/user", async (HttpContext r) => {
+	try {
+		// TODO: parse JWT to user information
+		var jwToken = r.Request.Headers["authorization"].FirstOrDefault();
+
+		return Results.Ok(jwToken);
+	} catch (Exception e) {
+		return Results.Problem("Parsing JWT error");
+	}
+});
+
+
 
 app.UseHttpsRedirection();
 
