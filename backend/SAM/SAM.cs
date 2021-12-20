c14eadd26a6f62662916f476cdc34111f7e4a6f8
Reinaldy Rafli <aldy505@tutanota.com>
2021-12-20 10:50:38 +0700

    chore: remove dominant level

--- backend/SAM/SAM.cs
+++ backend/SAM/SAM.cs
@@ -12,21 +12,18 @@ public class Sam {
 	/// <param name="condition">Whether this SAM test is before the coding test or after</param>
 	/// <param name="arousedLevel">From 1 to 10, how aroused is the user now</param>
 	/// <param name="pleasedLevel">From 1 to 10, how pleased is the user now</param>
-	/// <param name="dominantLevel">From 1 to 10, how dominant is the user now</param>
 	/// <param name="createdAt">When the data is created</param>
 	/// <param name="updatedAt">When the data is last updated</param>
 	protected Sam(User user,
 		ConditionType condition,
 		int arousedLevel,
 		int pleasedLevel,
-		int dominantLevel,
 		DateTime createdAt = default,
 		DateTime updatedAt = default) {
 		User = user;
 		Condition = condition;
 		ArousedLevel = arousedLevel;
 		PleasedLevel = pleasedLevel;
-		DominantLevel = dominantLevel;
 		CreatedAt = createdAt;
 		UpdatedAt = updatedAt;
 	}
@@ -35,7 +32,6 @@ public class Sam {
 	public ConditionType Condition { get; set; }
 	public int ArousedLevel { get; set; }
 	public int PleasedLevel { get; set; }
-	public int DominantLevel { get; set; }
 	public DateTime CreatedAt { get; init; }
 	public DateTime UpdatedAt { get; set; }
 }
