27c807edb03e83180ce1526503a83d3b4fe83176
Reinaldy Rafli <aldy505@tutanota.com>
2021-12-23 18:29:23 +0700

    feat(wip): tidying up stuffs

--- wranggler/main.go
+++ wranggler/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"encoding/json"
-	"github.com/influxdata/influxdb-client-go/v2"
-	"github.com/minio/minio-go/v7"
-	"github.com/minio/minio-go/v7/pkg/credentials"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/go-chi/chi/v5"
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
 func main() {
@@ -23,85 +24,38 @@ func main() {
 	if !ok {
 		log.Fatalln("INFLUX_ORG envar missing")
 	}
-	mhost, ok := os.LookupEnv("MINIO_HOST")
+	minioHost, ok := os.LookupEnv("MINIO_HOST")
 	if !ok {
 		log.Fatalln("MINIO_HOST envar missing")
 	}
-	maid, ok := os.LookupEnv("MINIO_ACCESS_ID")
+	minioID, ok := os.LookupEnv("MINIO_ACCESS_ID")
 	if !ok {
 		log.Fatalln("MINIO_ACCESS_ID envar missing")
 	}
-	maidsex, ok := os.LookupEnv("MINIO_SECRET_KEY")
+	minioSecret, ok := os.LookupEnv("MINIO_SECRET_KEY")
 	if !ok {
 		log.Fatalln("MINIO_SECRET_KEY envar missing")
 	}
 
 	influxConn := influxdb2.NewClient(influxHost, influxToken)
-
-	minioConn, err := minio.New(mhost, &minio.Options{
-		Creds:  credentials.NewStaticV4(maid, maidsex, ""),
+	defer influxConn.Close()
+	minioConn, err := minio.New(minioHost, &minio.Options{
+		Creds:  credentials.NewStaticV4(minioID, minioSecret, ""),
 		Secure: true,
 	})
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	http.HandleFunc("/fun-fact", func(w http.ResponseWritter, r *http.Request) {
-
-		type Member struct {
-			MemId string "json:`member_id`"
-		}
-
-		var x Member
-
-		err := json.NewDecoder(r.Body).Decode(&x)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		wpm := make(chan int8)
-		delRate := make(chan float32)
-
-		go func() {
-
-		}()
-		// aggregate WPM
-
-		go func() {
-
-		}()
-		// aggregate Delete keys
-
-		var result = struct {
-			Wpm     int8
-			DelRate float32
-		}{
-			<-wpm,
-			<-delRate,
-		}
-
-		res, _ := json.Marshall(result)
-
-		w.Write(res)
-
-		return
-	})
-
-	http.HandleFunc("/all-user-shit", func(w http.ResponseWritter, r *http.Request) {
-
-		return
-	})
-
-	http.HandleFunc("/ping", func(w http.ResponseWritter, r *http.Request) {
-
-		return
-	})
+	r := chi.NewRouter()
+	r.Get("/ping", Ping)
+	r.Post("/fun-fact", FunFact)
+	r.Post("/generate", GenerateFile)
 
 	potnum, ok := os.LookupEnv("PORT")
 	if ok {
-		http.ListenAndServe(":"+potnum, nil)
+		http.ListenAndServe(":"+potnum, r)
 	} else {
-		http.ListenAndServe(":4444", nil)
+		http.ListenAndServe(":4444", r)
 	}
 }
---
+++ wranggler/ping.go
@@ -0,0 +1,7 @@
+package main
+
+import "net/http"
+
+func Ping(w http.ResponseWriter, r *http.Request) {
+	return
+}
