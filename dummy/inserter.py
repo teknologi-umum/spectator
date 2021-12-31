63b6db87cce522ed4f6aa7531a2ab470d04705a9
Reinaldy Rafli <aldy505@tutanota.com>
2022-01-01 00:47:18 +0700

    feat: inserter functionality
    disclaimer: untested

--- dummy/inserter.py
+++ dummy/inserter.py
@@ -2,6 +2,7 @@ import os
 import json
 import influxdb_client
 from dotenv import load_dotenv
+from influxdb_client.client.write.point import Point
 from influxdb_client.client.write_api import ASYNCHRONOUS
 
 load_dotenv(".env")
@@ -33,20 +34,75 @@ def main():
     print("Environment variables are set. Let's connect to InfluxDB.")
 
     client = influxdb_client.InfluxDBClient(url=influx_url, token=influx_token, org=influx_org)
-    write_api = client.write_api(write_options=ASYNCHRONOUS)
 
-    # Write user personal first
-    with open("generated/user_personal.json") as f:
-        users = json.load(f)
-        # TODO: do a for loop to write the personal user
+    # Check whether the bucket exists
+    buckets_api = client.buckets_api()
+    session_events_bucket = buckets_api.find_bucket_by_name("session_events")
+    input_events_bucket = buckets_api.find_bucket_by_name("input_events")
 
-    with open("generated/events.json") as f:
-        events = json.load(f)
-        # TODO: do a for loop to write the events
-        # might as well use batch write
-        # see this: https://github.com/influxdata/influxdb-client-python#batching
-        # the idiomatic way maybe is by calling "with client.write_api() as write_api"
+    if session_events_bucket is None:
+        buckets_api.create_bucket(bucket_name="session_events", org=influx_org)
 
+    if input_events_bucket is None:
+        buckets_api.create_bucket(bucket_name="input_events", org=influx_org)
+
+    with client.write_api(write_options=ASYNCHRONOUS) as write_client:
+
+        # Write user personal first
+        with open("generated/user_personal.json") as f:
+            users = json.load(f)
+            for i in range(len(users)):
+                user = users[i]
+
+                point = Point(user["type"]) \
+                    .tag("session_id", user["session_id"]) \
+                    .field("student_number", user["student_number"]) \
+                    .field("hours_of_practice", user["hours_of_practice"]) \
+                    .field("years_of_experience", user["years_of_experience"]) \
+                    .field("familiar_language", user["familiar_language"])
+
+                write_client.write(
+                    bucket="session_events",
+                    org=influx_org,
+                    record=point,
+                )
+
+        with open("generated/events.json") as f:
+            events = json.load(f)
+            for i in range(len(events)):
+                event = events[i]
+
+                point = Point(event["type"]) \
+                    .tag("session_id", event["session_id"]) \
+                    .tag("question_number", event["question_number"])
+
+                if event["type"] == "coding_event_keystroke":
+                    point = point.field("key_char", event["key_char"]) \
+                        .field("key_code", event["key_code"]) \
+                        .field("shift", event["shift"]) \
+                        .field("alt", event["alt"]) \
+                        .field("control", event["control"]) \
+                        .field("meta", event["meta"]) \
+                        .field("unrelated_key", event["unrelated_key"]) \
+                        .time(event["time"])
+                elif event["type"] == "coding_event_mousemove":
+                    point = point.field("direction", event["direction"]) \
+                        .field("x_position", event["x_position"]) \
+                        .field("y_position", event["y_position"]) \
+                        .field("window_width", event["window_width"]) \
+                        .field("window_height", event["window_height"]) \
+                        .time(event["time"])
+                elif event["type"] == "coding_event_mouseclick":
+                    point = point.field("right_click", event["right_click"]) \
+                        .field("left_click", event["left_click"]) \
+                        .field("middle_click", event["middle_click"]) \
+                        .time(event["time"])
+
+                write_client.write(
+                    bucket="input_events",
+                    org=influx_org,
+                    record=point,
+                )
 
 if __name__ == "__main__":
     main()
