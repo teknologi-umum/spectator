d990e7a11d9b35e2e3fb2a3812f47001739cb91a
Reinaldy Rafli <aldy505@tutanota.com>
2022-01-02 21:28:16 +0700

    feat: answer for new questions

---
+++ assertion/javascript/question6.js
@@ -0,0 +1,51 @@
+{0}
+
+function main() {
+    const testCases = [
+        {
+            got: gradingStudents([73, 67, 38, 33]),
+            expected: [75, 67, 40, 33]
+        }
+    ];
+
+    const workingAnswer = (arr) => {
+        const out = [];
+        for (let i = 0; i < arr.length; i++) {
+            const grade = arr[i];
+
+            if (grade >= 38 && grade % 5 !== 0 && grade % 5 >= 3) {
+                out.push(grade + (5 - grade % 5));
+                continue;
+            }
+
+            out.push(grade);
+        }
+        return out;
+    }
+    const randomNumber = (a, b) => Math.floor(Math.random() * (b - a + 1) + a);
+    for (let i = 0; i < 9; i++) {
+        const input = [];
+        const arrLength = randomNumber(4, 20);
+        for (let j = 0; j < arrLength; j++) {
+            input.push(randomNumber(0, 100));
+        }
+
+        const expected = workingAnswer(input);
+        const got = gradingStudents(input);
+
+        testCases.push({ expected, got });
+    }
+
+    for (let i = 0; i < testCases.length; i++) {
+        const test = testCases[i];
+        if (test.expected.join(", ") === test.got.join(", ")) {
+            console.log(`# ${i+1} PASSING`);
+        } else {
+            console.log(`# ${i+1} FAILED`);
+            console.log(`> EXPECTED ${test.expected.join(", ")}`);
+            console.log(`> GOT ${test.got.join(", ")}`)
+        }
+    }
+}
+
+main();
