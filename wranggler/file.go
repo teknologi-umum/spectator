7cb24ade567106242d257e652f9c3385974a4d74
Y <47468136+ywnn@users.noreply.github.com>
2022-01-04 18:19:07 +0700

    test: struct test

--- wranggler/file.go
+++ wranggler/file.go
@@ -9,12 +9,67 @@ import (
 	"log"
 	"reflect"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
 
 	pb "worker/proto"
 )
+
+type MouseMovement struct {
+	SessionID      string    `json:"session_id" csv:"session_id"`
+	Type           string    `json:"type" csv:"type"`
+	QuestionNumber string    `json:"question_number" csv:"question_number"`
+	Direction      string    `json:"direction" csv:"direction"`
+	XPosition      int64     `json:"x_position" csv:"x_position"`
+	YPosition      int64     `json:"y_position" csv:"y_position"`
+	WindowWidth    int64     `json:"window_width" csv:"window_width"`
+	WindowHeight   int64     `json:"window_height" csv:"window_height"`
+	Timestamp      time.Time `json:"_timestap" csv:"_timestamp"`
+}
+
+type Keystroke struct {
+	SessionID      string    `json:"session_id" csv:"session_id"`
+	Type           string    `json:"type" csv:"type"`
+	QuestionNumber string    `json:"question_number" csv:"question_number"`
+	KeyChar        string    `json:"key_char" csv:"key_char"`
+	KeyCode        string    `json:"key_code" csv:"key_code"`
+	Shift          bool      `json:"shift" csv:"shift"`
+	Alt            bool      `json:"alt" csv:"alt"`
+	Control        bool      `json:"control" csv:"control"`
+	UnrelatedKey   bool      `json:"unrelated_key" csv:"control"`
+	Modifier       string    `json:"meta" csv:"meta"`
+	Timestamp      time.Time `json:"timestamp" csv:"timestamp"`
+}
+
+type MouseClick struct {
+	SessionID      string    `json:"session_id" csv:"session_id"`
+	Type           string    `json:"type" csv:"type"`
+	QuestionNumber string    `json:"question_number" csv:"question_number"`
+	RightClick     bool      `json:"right_click" csv:"right_click"`
+	LeftClick      bool      `json:"left_click" csv:"left_click"`
+	MiddleClick    bool      `json:"middle_click" csv:"middle_click"`
+	Timestamp      time.Time `json:"timestamp" csv:"timestamp"`
+}
+
+type PersonalInfo struct {
+	Type              string    `json:"type" csv:"type"`
+	SessionID         string    `json:"session_id" csv:"session_id"`
+	StudentNumber     string    `json:"student_number" csv:"student_number"`
+	HoursOfPractice   int64     `json:"hours_of_practice" csv:"hours_of_experience"`
+	YearsOfExperience int64     `json:"years_of_experience" csv:"years_of_experience"`
+	FamiliarLanguages string    `json:"familiar_languages" csv:"familliar_languages"`
+	Timestamp         time.Time `json:"timestamp" csv:"timestamp"`
+}
+
+type SamTest struct {
+	SessionID    string    `json:"session_id" csv:"session_id"`
+	Type         string    `json:"type" csv:"type"`
+	ArousedLevel int64     `json:"aroused_level" csv:"aroused_level"`
+	PleasedLevel int64     `json:"pleased_level" csv:"pleased_level"`
+	Timestamp    time.Time `json:"timestamp" csv:"timestamp"`
+}
 
 // GenerateFile is the handler for generating file into CSV and JSON based on
 // the input data (which only contains the Session ID).
@@ -47,10 +102,10 @@ func (d *Dependency) CreateFile(sessionID uuid.UUID) {
 	queryAPI := d.DB.QueryAPI(d.DBOrganization)
 
 	// keystroke and mouse
-	// keystrokeMouseRows
-	_, err := queryAPI.Query(
+	keystrokeMouseRows, err := queryAPI.Query(
 		ctx,
 		`from(bucket: "`+BucketInputEvents+`")
+		|> range(start: 0)
 		|> filter(fn : (r) => r["session_id"] == "`+sessionID.String()+`
 		|> filter(fn : (r) => r["_measurement"] == "coding_event_keystroke")
 		`,
@@ -60,6 +115,55 @@ func (d *Dependency) CreateFile(sessionID uuid.UUID) {
 		// for now, we'll just do this:
 		log.Println(err)
 		return
+	}
+
+	//var lastTableIndex int = -1
+	output := []Keystroke{}
+	temp := Keystroke{}
+	for keystrokeMouseRows.Next() {
+		unmarshaledRow, err := UnmarshalInfluxRow(keystrokeMouseRows.Record().String())
+		if err != nil {
+			return
+		}
+
+		// tableStr, ok := unmarshaledRow["table"].(string)
+		// if !ok {
+		// 	continue
+		// }
+
+		// table, err := strconv.Atoi(tableStr)
+		// if err != nil {
+		// 	return
+		// }
+		// if table == lastTableIndex {
+		switch unmarshaledRow["_field"].(string) {
+		case "key_char":
+			temp.KeyChar = unmarshaledRow["_value"].(string)
+		case "key_code":
+			temp.KeyCode = unmarshaledRow["_value"].(string)
+		case "shift":
+			temp.Shift = unmarshaledRow["_value"].(bool)
+		case "alt":
+			temp.Alt = unmarshaledRow["_value"].(bool)
+		case "control":
+			temp.Control = unmarshaledRow["_value"].(bool)
+		case "unrelated_key":
+			temp.UnrelatedKey = unmarshaledRow["_value"].(bool)
+		case "meta":
+			temp.Modifier = unmarshaledRow["_value"].(string)
+		}
+		// } else {
+		// clear the last temp, but check if its less than zero
+		// if lastTableIndex >= 0 {
+		// 	output = append(output, temp)
+		// }
+		// create a new one
+		temp.QuestionNumber = unmarshaledRow["question_number"].(string)
+		temp.SessionID = unmarshaledRow["session_id"].(string)
+		temp.Timestamp = keystrokeMouseRows.Record().Time()
+		// lastTableIndex = table
+		// }
+		output = append(output, temp)
 	}
 
 	//keystrokeMouseClickRows
@@ -137,37 +241,28 @@ func (d *Dependency) CreateFile(sessionID uuid.UUID) {
 	// nil bucket.
 }
 
-type MouseMovement struct {
-	SessionID      string  `json:"session_id"`
-	Type           string  `json:"type"`
-	QuestionNumber int64   `json:"question_number"`
-	Direction      *string `json:"direction"`
-	XPosition      *int64  `json:"x_position"`
-	YPosition      *int64  `json:"y_position"`
-	WindowWidth    *int64  `json:"window_width"`
-	WindowHeight   *int64  `json:"window_height"`
-}
+func UnmarshalInfluxRow(row string) (map[string]interface{}, error) {
+	// because csv.NewReader() accepts io.Reader, we'll create one from strings pkg
+	input := strings.NewReader(row)
+	reader := csv.NewReader(input)
+	reader.LazyQuotes = true
+	reader.TrimLeadingSpace = true
+	records, err := reader.Read()
+	if err != nil {
+		return map[string]interface{}{}, fmt.Errorf("reading row value to csv: %v", err)
+	}
 
-type Keystroke struct {
-	SessionID      string `json:"session_id"`
-	Type           string `json:"type"`
-	QuestionNumber string `json:"question_number"`
-	KeyChar        string `json:"key_char"`
-	KeyCode        string `json:"key_code"`
-	Shift          bool   `json:"shift"`
-	Alt            bool   `json:"alt"`
-	Control        bool   `json:"control"`
-	UnrelatedKey   bool   `json:"unrelated_key"`
-	Modifier       string `json:"modifier"`
-}
+	// find records length
+	// because it's a jagged array, we'll do a nested one
+	var recordsLength = len(records)
 
-type MouseClick struct {
-	SessionID      string `json:"session_id"`
-	Type           string `json:"type"`
-	QuestionNumber string `json:"question_number"`
-	RightClick     *bool  `json:"right_click"`
-	LeftClick      *bool  `json:"left_click"`
-	MiddleClick    *bool  `json:"middle_click"`
+	output := make(map[string]interface{}, recordsLength)
+	for _, rec := range records {
+		kv := strings.Split(rec, ":")
+		output[kv[0]] = kv[1]
+	}
+
+	return output, nil
 }
 
 func ConvertDataToJSON(input []interface{}) ([]byte, error) {
