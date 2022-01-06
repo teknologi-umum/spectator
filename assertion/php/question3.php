4c1fcd752501f0bbc7711a5bc23d60adafc487ea
Rubi Chandraputra <rubichandrap@gmail.com>
2022-01-06 12:51:49 +0700

    feat: assertion php

---
+++ assertion/php/question3.php
@@ -0,0 +1,51 @@
+<?php
+
+{0}
+
+function main(): void {
+    $test_cases = [
+        [
+            "got" => isSameNumber(100, 212),
+            "expected" => false
+        ],
+        [
+            "got" => isSameNumber(25, 25),
+            "expected" => true
+        ],
+    ];
+
+    for ($i = 0; $i < 4; $i++) {
+        $a = rand(0, 9999);
+        $b = rand(0, 9999);
+        $expected = $a === $b;
+        $got = isSameNumber($a, $b);
+        array_push($test_cases, [
+            "got" => $got,
+            "expected" => $expected
+        ]);
+    }
+
+    for ($i = 0; $i < 4; $i++) {
+        $a = rand(0, 9999);
+        $expected = true;
+        $got = isSameNumber($a, $a);
+        array_push($test_cases, [
+            "got" => $got,
+            "expected" => $expected
+        ]);
+    }
+
+    for ($i = 0; $i < count($test_cases); $i++) {
+        $test = $test_cases[$i];
+
+        if ($test["got"] === $test["expected"]) {
+            echo "# ". $i + 1 . " PASSING"; 
+        } else {
+            echo "# " . $i + 1 . " FAILED";
+            echo "> EXPECTED " . $test["expected"];
+            echo "> GOT " . $test["got"];
+        }
+    }
+}
+
+main();
