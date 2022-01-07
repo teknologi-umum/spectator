d20fdb78ecd4906153c4242fb928b4c24df0d75e
elianiva <dicha.arkana03@gmail.com>
2022-01-07 14:02:12 +0700

    fix(assertion): double free, wrong logic
    BRUH IT WORRKSSSSKSKJDLKFF!1!!!!11

--- assertion/c/question6.c
+++ assertion/c/question6.c
@@ -21,8 +21,8 @@ int* __workingAnswer(int size, int* input) {
 }
 
 typedef struct TestCase {
-    int* expected;
-    int* got;
+    char* expected;
+    char* got;
 } TestCase;
 
 // creates a random number between min and max
@@ -33,55 +33,68 @@ long __randomNumber(int min, long max) {
 int* genArray(int len) {
     int* result = malloc(len * sizeof(int));
     for (int i = 0; i < len; i++) {
-        result[i] = __randomNumber(0, 100);
+        result[i] = __randomNumber(10, 100);
     }
     return result;
 }
 
 // this is poorman's implementation
-char* arrayToString(int* arr) {
-    char* result = malloc(sizeof(arr));
+char* arrayToString(int len, int* arr) {
+    char* result = malloc(255 * sizeof(char));
 
-    for (int i = 0, len = sizeof(arr); i < len; i++) {
-        sprintf(result, "%d", arr[i]);
+    sprintf(&result[0], "%c", '[');
+    for (int i = 0; i < len - 1; i++) {
+        sprintf(&result[strlen(result)], "%d, ", arr[i]);
     }
+    sprintf(&result[strlen(result)], "%d%c", arr[len], ']');
 
     return result;
 }
 
 int main() {
     srand(time(0));
+
     TestCase testCases[10];
 
     int answer[] = {75, 67, 40, 33};
-    testCases[0].expected = answer;
-    testCases[0].got = calculateGrade(4, answer);
+
+    int* input = malloc(4 * sizeof(int));
+    input[0] = 73;
+    input[1] = 67;
+    input[2] = 38;
+    input[3] = 33;
+
+    testCases[0].expected = arrayToString(4, answer);
+    testCases[0].got = arrayToString(4, calculateGrade(4, input));
 
     for (int i = 1; i < 10; i++) {
         int len = __randomNumber(4, 20);
+
         int* input = genArray(len);
+        // if I don't add +1 here it'll lead to UB, idk why but this thing works
+        int* input2 = malloc(len * sizeof(int) + 1);
+        memcpy(input2, input, len * sizeof(int) + 1);
+
         int* expected = __workingAnswer(len, input);
-        int* got = calculateGrade(len, input);
-        testCases[i].expected = expected;
-        testCases[i].got = got;
+        int* got = calculateGrade(len, input2);
+
+        testCases[i].expected = arrayToString(len, expected);
+        testCases[i].got = arrayToString(len, got);
     }
 
     for (unsigned int i = 0, len = sizeof(testCases) / sizeof(TestCase); i < len; i++) {
         TestCase test = testCases[i];
 
-        char* got = arrayToString(test.got);
-        char* expected = arrayToString(test.expected);
-
-        if (strcmp(got, expected) == 1) {
+        if (strcmp(test.got, test.expected) == 0) {
             printf("# %d PASSING\n", i + 1);
         } else {
             printf("# %d FAILED\n", i + 1);
-            printf("> EXPECTED %s\n", expected);
-            printf("> GOT %s\n", got);
+            printf("> EXPECTED %s\n", test.expected);
+            printf("> GOT %s\n", test.got);
         }
 
-        free(got);
-        free(expected);
+        free(test.got);
+        free(test.expected);
     }
     return 0;
 }
