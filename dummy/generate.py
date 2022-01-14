7ca5ddf6096348e19ee82ecd962d4d60d1a74626
Y <47468136+ywnn@users.noreply.github.com>
2022-01-14 23:30:04 +0700

    feat: reinaldy's sama quest (1/?)

--- dummy/generate.py
+++ dummy/generate.py
@@ -27,6 +27,10 @@ def write_into_file(filename: str, data):
 def main():
     users: list[dict[str, any]] = []
 
+    # begini bukan
+    number_of_data = 5
+    date_range = 2
+
     for _ in range(5):
         user = generate_user()
         users.append(user)
@@ -44,32 +48,33 @@ def main():
         # Generate 2 random dates that are close to each other
         date_start_int: int = random_date(datetime(2021, 6, 1, 0, 0, 0), datetime(2021, 12, 29, 23, 59, 59))
         date_start: datetime = datetime.fromtimestamp(date_start_int)
-        additional_duration: timedelta = timedelta(minutes=random.randint(20, 90))
+        additional_duration: timedelta = timedelta(minutes=random.randint(20, (20 + date_range)))
         date_ends: datetime = datetime.fromtimestamp(date_start_int + additional_duration.total_seconds())
 
-        for _ in range(random.randint(200, 500)):
-            # Generate random number between 1 to 3
-            rand = random.randint(1, 4)
-            if rand == 1:
-                # Generate a keystroke event
-                event = generate_event_keystroke(current_session, date_start, date_ends)
-                current_events.append(event)
-                continue
-            elif rand == 2:
-                # Generate a mouse move event
-                event = generate_event_mousemove(current_session, date_start, date_ends)
-                current_events.append(event)
-                continue
-            elif rand == 3:
-                # Generate a window resize event
-                event = generate_event_window_sized(current_session, date_start, date_ends)
-                current_events.append(event)
-                continue
-            else:
-                # Generate a mouse click event
-                event = generate_event_mouseclick(current_session, date_start, date_ends)
-                current_events.append(event)
-                continue
+        for _ in range(1..number_of_data):
+            for _ in range(random.randint(200, 500)):
+                # Generate random number between 1 to 3
+                rand = random.randint(1, 4)
+                if rand == 1:
+                    # Generate a keystroke event
+                    event = generate_event_keystroke(current_session, date_start, date_ends)
+                    current_events.append(event)
+                    continue
+                elif rand == 2:
+                    # Generate a mouse move event
+                    event = generate_event_mousemove(current_session, date_start, date_ends)
+                    current_events.append(event)
+                    continue
+                elif rand == 3:
+                    # Generate a window resize event
+                    event = generate_event_window_sized(current_session, date_start, date_ends)
+                    current_events.append(event)
+                    continue
+                else:
+                    # Generate a mouse click event
+                    event = generate_event_mouseclick(current_session, date_start, date_ends)
+                    current_events.append(event)
+                    continue
 
         # Add the current events to the list of events
         events.extend(current_events)
