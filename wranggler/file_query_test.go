138c36e5c9fcef3988256b15405505c07011d037
Reinaldy Rafli <aldy505@tutanota.com>
2022-01-16 23:56:16 +0700

    test: scaffold test for file_query

---
+++ wranggler/file_query_test.go
@@ -0,0 +1,30 @@
+package main_test
+
+import "testing"
+
+func TestQueryKeystrokes(t *testing.T) {
+	// TODO:
+	// 1. insert some fake data into the influx db
+	// 2. query the data with the function
+	// 3. compare the length of the result and the length of fake data
+	// add another test (maybe a subtest, or another test function)
+	// that checks if there is no data to be queried. 
+	// we must check if that (rare and edgy) event happen,
+	// so what would the software react?
+}
+
+func TestQueryMouseClick(t *testing.T) {
+	// TODO
+}
+
+func TestQueryMouseMove(t *testing.T) {
+	// TODO
+}
+
+func TestQueryPersonalInfo(t *testing.T) {
+	// TODO
+}
+
+func TestQuerySamTest(t *testing.T) {
+	// TODO
+}
