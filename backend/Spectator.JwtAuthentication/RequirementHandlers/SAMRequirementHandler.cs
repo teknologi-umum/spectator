535b563d9a2f706c4fe7207376aca3d024f1cc50
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2022-02-11 20:01:26 +0700

    Fail with message

--- backend/Spectator.JwtAuthentication/RequirementHandlers/SAMRequirementHandler.cs
+++ backend/Spectator.JwtAuthentication/RequirementHandlers/SAMRequirementHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Spectator.DomainModels.SessionDomain;
 using Spectator.JwtAuthentication.Requirements;
@@ -19,26 +20,30 @@ namespace Spectator.JwtAuthentication.RequirementHandlers {
 				var tokenPayload = TokenPayload.FromClaimsPrincipal(context.User);
 				if (!_sessionSilo.TryGet(tokenPayload.SessionId, out var sessionStore)
 					|| sessionStore.State is not RegisteredSession registeredSession) {
-					context.Fail();
+					context.Fail(new AuthorizationFailureReason(this, "Session not found"));
 					return Task.CompletedTask;
 				}
 
-				if (requirement.BeforeTest.HasValue
-					&& requirement.BeforeTest.Value != (registeredSession.BeforeExamSAM != null)) {
-					context.Fail();
+				if (requirement.BeforeExam.HasValue
+					&& requirement.BeforeExam.Value != (registeredSession.BeforeExamSAM != null)) {
+					context.Fail(new AuthorizationFailureReason(this, requirement.BeforeExam.Value ? "Before exam SAM not submitted" : "Before exam SAM already submitted"));
 					return Task.CompletedTask;
 				}
 
-				if (requirement.AfterTest.HasValue
-					&& requirement.AfterTest.Value != (registeredSession.AfterExamSAM != null)) {
-					context.Fail();
+				if (requirement.AfterExam.HasValue
+					&& requirement.AfterExam.Value != (registeredSession.AfterExamSAM != null)) {
+					context.Fail(new AuthorizationFailureReason(this, requirement.AfterExam.Value ? "After exam SAM not submitted" : "After exam SAM already submitted"));
 					return Task.CompletedTask;
 				}
 
 				context.Succeed(requirement);
 				return Task.CompletedTask;
-			} catch {
+			} catch (Exception exc) {
+#if DEBUG
+				context.Fail(new AuthorizationFailureReason(this, exc.Message));
+#else
 				context.Fail();
+#endif
 				return Task.CompletedTask;
 			}
 		}
