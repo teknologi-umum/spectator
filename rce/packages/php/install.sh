68cb203f3f856a2ef683b63e08008ff56d5dc87d
elianiva <dicha.arkana03@gmail.com>
2022-02-26 22:18:29 +0700

    fix: use curl so we don't need to install wget

--- rce/packages/php/install.sh
+++ rce/packages/php/install.sh
@@ -2,7 +2,7 @@
 
 apt-get -y install apt-transport-https lsb-release ca-certificates curl
 
-wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+curl -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 
 sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
 
