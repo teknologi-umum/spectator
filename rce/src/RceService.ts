3a3c178623e3e101916fc2737f995f95179a2365
elianiva <dicha.arkana03@gmail.com>
2022-02-27 13:18:33 +0700

    refactor: change esbuild to tsc

--- rce/src/RceService.ts
+++ rce/src/RceService.ts
@@ -1,8 +1,10 @@
 import { ICodeExecutionEngineService } from "@/stub/rce.grpc-server";
-import { sendUnaryData } from "@grpc/grpc-js";
+import { sendUnaryData, UntypedHandleCall } from "@grpc/grpc-js";
 import { PingResponse } from "@/stub/rce";
 
 export class RceServiceImpl implements ICodeExecutionEngineService {
+  [name: string]: UntypedHandleCall;
+
   public listRuntimes() {
     // TODO: implementation
   }
