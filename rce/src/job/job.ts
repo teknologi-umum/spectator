f0d53df104d5a90da7dd4bac8040cf7a42767bf4
Reinaldy Rafli <aldy505@tutanota.com>
2022-02-27 14:58:14 +0700

    feat: change config to reflect dynamic file names

--- rce/src/job/job.ts
+++ rce/src/job/job.ts
@@ -49,6 +49,8 @@ export class Job implements JobPrerequisites {
         }
 
         const fileName = path.basename(filePath);
+        // TODO: append nice and prlimit to the runCommand below
+        // TODO: run as user. see https://www.cyberciti.biz/open-source/command-line-hacks/linux-run-command-as-different-user/
         const buildCommand = this.runtime.buildCommand.map(arg => arg.replace("{file}", fileName));
         const buildCommandOutput = this.executeCommand(buildCommand);
         if (buildCommandOutput.exitCode !== 0 || buildCommandOutput.stderr) {
@@ -60,7 +62,12 @@ export class Job implements JobPrerequisites {
         const fileName = path.basename(filePath);
         // TODO: append nice and prlimit to the runCommand below
         // TODO: run as user. see https://www.cyberciti.biz/open-source/command-line-hacks/linux-run-command-as-different-user/
-        const runCommand = this.runtime.runCommand.map(arg => arg.replace("{file}", fileName));
+        const runCommand = this.runtime.runCommand.map(
+            arg => arg.replace(
+                "{file}",
+                fileName.replace(`.${this.runtime.extension}`, "")
+            )
+        );
         return this.executeCommand(runCommand);
     }
 
