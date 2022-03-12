cf9c14b581aa0dc908d381ff0d4fed9df45a9018
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2022-03-12 14:42:48 +0700

    Add InputEventRepository

---
+++ backend/Spectator.DomainServices/InputDomain/InputServices.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Spectator.DomainEvents.InputDomain;
+using Spectator.Repositories;
+
+namespace Spectator.DomainServices.InputDomain {
+	public class InputServices {
+		private readonly IInputEventRepository _inputEventRepository;
+
+		public InputServices(
+			IInputEventRepository inputEventRepository
+		) {
+			_inputEventRepository = inputEventRepository;
+		}
+
+		public Task AddInputEventAsync(InputEventBase @event) {
+			return _inputEventRepository.AddEventAsync(@event);
+		}
+	}
+}
