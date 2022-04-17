3f1f6f61768749f448f5bee763b2bc7765256ab8
Reinaldy Rafli <aldy505@tutanota.com>
2022-04-17 21:05:15 +0700

    feat: create bucket on startup if not exists

---
+++ backend/Spectator.RepositoryDALs/InfluxDbInitialization.cs
@@ -0,0 +1,28 @@
+﻿using InfluxDB.Client;
+using InfluxDB.Client.Core.Exceptions;
+namespace Spectator.RepositoryDALs;
+
+public static class InfluxDbInitialization {
+	 public static async Task InitializeAsync(InfluxDBClient client, InfluxDbOptions options) {
+		 var organization = await client.GetOrganizationsApi()
+			 .FindOrganizationsAsync(limit: 1, org: options.Org);
+
+		 if (organization.Count == 0) {
+			 throw new Exception("Organization was not found");
+		 }
+
+		 try {
+			 await client.GetBucketsApi().FindBucketByNameAsync(options.InputEventsBucket);
+		 } catch (NotFoundException) {
+			 await client.GetBucketsApi()
+				 .CreateBucketAsync(options.InputEventsBucket, organization[0]);
+		 }
+
+		 try {
+			 await client.GetBucketsApi().FindBucketByNameAsync(options.SessionEventsBucket);
+		 } catch (NotFoundException) {
+			 await client.GetBucketsApi()
+				 .CreateBucketAsync(options.SessionEventsBucket, organization[0]);
+		 }
+	 }
+}
--- backend/Spectator.RepositoryDALs/ServiceCollectionExtensions.cs
+++ backend/Spectator.RepositoryDALs/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using InfluxDB.Client;
+using InfluxDB.Client.Core.Exceptions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Spectator.Repositories;
@@ -8,12 +9,14 @@ using Spectator.RepositoryDALs.Mapper;
 namespace Spectator.RepositoryDALs {
 	public static class ServiceCollectionExtensions {
 		public static IServiceCollection AddRepositoryDALs(this IServiceCollection services) {
-			services.AddSingleton(serviceProvider => {
+			services.AddSingleton(async serviceProvider => {
 				var options = serviceProvider.GetRequiredService<IOptions<InfluxDbOptions>>().Value;
-				return InfluxDBClientFactory.Create(
+				var client =  InfluxDBClientFactory.Create(
 					url: options.Url ?? throw new InvalidOperationException("InfluxDbOptions:Url is required"),
 					token: options.Token ?? throw new InvalidOperationException("InfluxDbOptions:Token is required")
 				);
+				await InfluxDbInitialization.InitializeAsync(client, options);
+				return client;
 			});
 			services.AddSingleton<IDomainObjectMapper, DomainObjectMapper>();
 			services.AddTransient<IQuestionRepository, QuestionRepositoryDAL>();
