284287bd9c048bb401ba8051f6f57acd8da1387e
elianiva <dicha.arkana03@gmail.com>
2022-04-21 07:58:28 +0700

    feat(frontend): delegate upload to web workers

--- frontend/src/events/video.ts
+++ frontend/src/events/video.ts
@@ -1,26 +1,16 @@
-import { loggerInstance } from "@/spoke/logger";
-import { LogLevel } from "@microsoft/signalr";
+import type { UploadRequest } from "@/workers/uploader.worker";
+import UploaderWorker from "@/workers/uploader.worker.ts?worker";
+
+const worker = new UploaderWorker();
 
 export function uploadVideo(accessToken: string, startedAt: number) {
-  return async (e: BlobEvent) => {
-    const formData = new FormData();
-    formData.append("startedAt", startedAt.toString());
-    formData.append("stoppedAt", Date.now().toString());
-    formData.append("file", e.data);
-
-    try {
-      await fetch(import.meta.env.VITE_VIDEO_STREAM_URL, {
-        method: "POST",
-        headers: {
-          "Content-Length": e.data.size.toString(),
-          "Authorization": accessToken
-        },
-        body: formData
-      });
-    } catch (err) {
-      if (err instanceof Error) {
-        await loggerInstance.log(LogLevel.Error, err.message);
-      }
-    }
+  return (e: BlobEvent) => {
+    const data: UploadRequest = {
+      blob: e.data,
+      size: e.data.size.toString(),
+      accessToken: accessToken,
+      startedAt: startedAt
+    };
+    worker.postMessage(data);
   };
 }
