c61f2dc3fb3511f99884ed336a0f0115556e98f7
elianiva <dicha.arkana03@gmail.com>
2022-04-22 12:47:53 +0700

    fix(frontend): use `super` instead of `this`

--- frontend/src/spoke/eventSpoke.ts
+++ frontend/src/spoke/eventSpoke.ts
@@ -9,32 +9,32 @@ import {
 
 class EventSpoke extends SpokeBase {
   public async mouseScrolled(request: MouseScrollInfo) {
-    await this._startIfDisconnected();
+    await super._startIfDisconnected();
     return this._hubConnection.invoke("LogMouseScrolledAsync", request);
   }
 
   public async mouseUp(request: MouseClickInfo) {
-    await this._startIfDisconnected();
+    await super._startIfDisconnected();
     return this._hubConnection.invoke("LogMouseUpAsync", request);
   }
 
   public async mouseDown(request: MouseClickInfo) {
-    await this._startIfDisconnected();
+    await super._startIfDisconnected();
     return this._hubConnection.invoke("LogMouseDownAsync", request);
   }
 
   public async mouseMoved(request: MouseMoveInfo) {
-    await this._startIfDisconnected();
+    await super._startIfDisconnected();
     return this._hubConnection.invoke("LogMouseMovedAsync", request);
   }
 
   public async keystroke(request: KeystrokeInfo) {
-    await this._startIfDisconnected();
+    await super._startIfDisconnected();
     return this._hubConnection.invoke("LogKeystrokeAsync", request);
   }
 
   public async windowResized(request: WindowSizeInfo) {
-    await this._startIfDisconnected();
+    await super._startIfDisconnected();
     return this._hubConnection.invoke("LogWindowSizedAsync", request);
   }
 }
