e6da31474aac503b6f8e5ea3502d75c744354ef0
elianiva <dicha.arkana03@gmail.com>
2022-04-22 08:23:14 +0700

    test(frontend): add more session tests

--- frontend/src/tests/redux/jwtSlice.test.ts
+++ frontend/src/tests/redux/jwtSlice.test.ts
@@ -1,11 +1,16 @@
 import { expect, test } from "vitest";
-import reducer, { setAccessToken } from "@/store/slices/sessionSlice";
+import reducer, {
+  removeSessionId,
+  setAccessToken,
+  setSessionId
+} from "@/store/slices/sessionSlice";
 
 const FAKE_JWT =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdHVkZW50SWQiOiIxMjM0NTY3ODkwIiwiaWF0IjoxNjQxMDMxMTYwODE3LCJleHAiOjYwMDAwfQ==.ySxmfXC7SSP4NR7Go2qitririWvL-vWMLZUDjY0w6U8";
 
 const initialState = {
   accessToken: null,
+  sessionId: null,
   firstSAMSubmitted: false,
   secondSAMSubmitted: false,
   tourCompleted: {
@@ -22,8 +27,25 @@ test("should return the initial state", () => {
 test("should be able to set access token", () => {
   expect(reducer(initialState, setAccessToken(FAKE_JWT))).toEqual({
     ...initialState,
-    accessToken: FAKE_JWT,
-    firstSAMSubmitted: false,
-    secondSAMSubmitted: false
+    accessToken: FAKE_JWT
+  });
+});
+
+test("should be able to set session ID", () => {
+  expect(reducer(initialState, setSessionId("fake-session-uuid"))).toEqual({
+    ...initialState,
+    sessionId: "fake-session-uuid"
+  });
+});
+
+test("should be able to remove session ID", () => {
+  expect(
+    reducer(
+      { ...initialState, sessionId: "fake-session-id" },
+      removeSessionId()
+    )
+  ).toEqual({
+    ...initialState,
+    sessionId: null
   });
 });
