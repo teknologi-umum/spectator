6d0cca5f66f049cb63e7c158555773f09d02cc91
Reinaldy Rafli <aldy505@tutanota.com>
2022-04-24 00:40:53 +0700

    fix: cleaning up worker logic and misleads

--- worker/buckets.go
+++ worker/buckets.go
@@ -14,12 +14,12 @@ func (d *Dependency) prepareBuckets(ctx context.Context) error {
 	if err != nil && err.Error() != "bucket '"+common.BucketInputStatisticEvents+"' not found" {
 		orgDomain, err := orgsAPI.FindOrganizationByName(ctx, d.DBOrganization)
 		if err != nil {
-			return fmt.Errorf("failed to find organization: %v", err)
+			return fmt.Errorf("failed to find organization: %w", err)
 		}
 
 		_, err = bucketsAPI.CreateBucketWithName(ctx, orgDomain, common.BucketInputStatisticEvents)
 		if err != nil {
-			return fmt.Errorf("failed to create bucket: %v", err)
+			return fmt.Errorf("failed to create bucket: %w", err)
 		}
 	}
 
@@ -27,12 +27,12 @@ func (d *Dependency) prepareBuckets(ctx context.Context) error {
 	if err != nil && err.Error() != "bucket '"+common.BucketFileEvents+"' not found" {
 		orgDomain, err := orgsAPI.FindOrganizationByName(ctx, d.DBOrganization)
 		if err != nil {
-			return fmt.Errorf("failed to find organization: %v", err)
+			return fmt.Errorf("failed to find organization: %w", err)
 		}
 
 		_, err = bucketsAPI.CreateBucketWithName(ctx, orgDomain, common.BucketFileEvents)
 		if err != nil {
-			return fmt.Errorf("failed to create bucket: %v", err)
+			return fmt.Errorf("failed to create bucket: %w", err)
 		}
 	}
 
