fe4a8378d93286e7a6ea5beb3879ea3810992876
elianiva <dicha.arkana03@gmail.com>
2022-05-09 11:26:54 +0700

    refactor(frontend): don't use ternary in routes

--- frontend/src/hoc/CoercedRoute.tsx
+++ frontend/src/hoc/CoercedRoute.tsx
@@ -4,24 +4,35 @@ import { useAppSelector } from "@/store";
 import { useMemo } from "react";
 
 export const CoercedRoute = () => {
-  const { accessToken, firstSAMSubmitted, secondSAMSubmitted } = useAppSelector((state) => state.session);
+  const { accessToken, firstSAMSubmitted, secondSAMSubmitted } = useAppSelector(
+    (state) => state.session
+  );
   const { studentNumber } = useAppSelector((state) => state.personalInfo);
   const { deadlineUtc, questions } = useAppSelector((state) => state.editor);
   const { examResult } = useAppSelector((state) => state.examResult);
   const location = useLocation();
 
-  const validPath = useMemo(
-    () => accessToken === null || studentNumber === null || studentNumber === ""
-      ? "/"
-      : firstSAMSubmitted !== true || deadlineUtc === null || questions === null
-        ? "/sam-test"
-        : examResult === null
-          ? "/coding-test"
-          : secondSAMSubmitted === null
-            ? "/sam-test"
-            : "/fun-fact",
-    [accessToken, studentNumber, firstSAMSubmitted, deadlineUtc, questions, examResult, secondSAMSubmitted]
-  );
+  const validPath = useMemo(() => {
+    if (accessToken === null || studentNumber === null || studentNumber === "") {
+      return "/";
+    }
+
+    if (firstSAMSubmitted === false || deadlineUtc === null || questions === null) {
+      return "/sam-test";
+    }
+
+    if (examResult === null) return "/coding-test";
+    if (secondSAMSubmitted === null) return "/sam-test";
+    return "/fun-fact";
+  }, [
+    accessToken,
+    studentNumber,
+    firstSAMSubmitted,
+    deadlineUtc,
+    questions,
+    examResult,
+    secondSAMSubmitted
+  ]);
 
   if (location.pathname !== validPath) {
     return <Navigate to={validPath} />;
