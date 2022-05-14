f2abc269a13f9cf6d8fcdedcf6a58bc4d30c7d57
elianiva <dicha.arkana03@gmail.com>
2022-05-14 09:54:41 +0700

    feat(frontend): list all downloadable files

--- frontend/src/pages/secret/Download.tsx
+++ frontend/src/pages/secret/Download.tsx
@@ -1,17 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { LocaleButton, ThemeButton } from "@/components/TopBar";
 import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Badge,
   Box,
   Button,
   Flex,
   Heading,
-  HStack,
-  Table,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr
+  Text as ChakraText
 } from "@chakra-ui/react";
 import { useColorModeValue } from "@/hooks";
 import { useTranslation } from "react-i18next";
@@ -31,14 +31,59 @@ const FAKE_DATA: FakeData[] = [...Array(20).fill(0)].map((_, n) => ({
   fileUrlCsv: "https://fake.url.dont.go.here"
 }));
 
+interface FileEntry {
+  csvFileUrl: string;
+  jsonFileUrl: string;
+  sessionId: string;
+  studentNumber: string;
+}
+
+interface GroupedFileEntry {
+  [key: string]: FileEntry[];
+}
+
+// the response from backend is not very humanised, we'll transform this
+// so it's easier to read by humans
+const HUMANISED_FILENAME: Record<string, string> = {
+  solutionaccepted: "Solution Accepted",
+  solutionrejected: "Solution Rejected",
+  personalinfo: "Personal Info",
+  sambefore: "Before Exam SAM Test",
+  samafter: "After Exam SAM Test",
+  examstarted: "Exam Started",
+  examended: "Exam Ended",
+  examforfeited: "Exam Forfeited",
+  examidereloaded: "IDE Reloaded",
+  deadlinepassed: "Deadline Passed",
+  mousescrolled: "Mouse Scrolled"
+};
+
+const toCapitalised = (str: string) =>
+  str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+
+function parseFileName(url: string) {
+  // example url: /public/51946418_solution_events_solutionaccepted.csv
+  return url
+    .replace(/\/public\//, "") // remove `/public/` prefix
+    .split(".")[0] // remove the file extension
+    .split("_") // remove `{sessionId}_` prefix
+    .slice(1)
+    .map((word) => HUMANISED_FILENAME[word]?.split(" ") || word) // humanise the ending, it's a bit hard for human to read
+    .map((word) => {
+      if (typeof word === "string") return toCapitalised(word);
+      return word.map(toCapitalised).join(" ");
+    }) // capitalise
+    .join(" ");
+}
+
 export default function Download() {
+  const [files, setFiles] = useState<GroupedFileEntry>({});
   const { sessionId } = useAppSelector((state) => state.session);
   const { t } = useTranslation();
 
   const boxBg = useColorModeValue("white", "gray.700", "gray.800");
   const bg = useColorModeValue("gray.100", "gray.800", "gray.900");
   const fg = useColorModeValue("gray.800", "gray.100", "gray.100");
-  const fgDarker = useColorModeValue("gray.700", "gray.400", "gray.400");
 
   useEffect(() => {
     (async () => {
@@ -53,8 +98,15 @@ export default function Download() {
             body: JSON.stringify({ sessionId })
           }
         );
-        const data = await response.json();
-        console.log(data);
+        const data: FileEntry[] = await response.json();
+        const dataGroupedByStudentNumber = data.reduce((acc, curr) => {
+          if (acc[curr.studentNumber] === undefined) {
+            acc[curr.studentNumber] = [];
+          }
+          acc[curr.studentNumber].push(curr);
+          return acc;
+        }, {} as GroupedFileEntry);
+        setFiles(dataGroupedByStudentNumber);
       } catch (err) {
         if (err instanceof Error) {
           loggerInstance.log(LogLevel.Error, err.message);
@@ -77,37 +129,72 @@ export default function Download() {
         <Heading mb="8" size="lg" textAlign="center" fontWeight="700">
           List of Students Data
         </Heading>
-        <Box bg={boxBg} rounded="md" maxH="32rem" overflowY="auto">
-          <Table variant="simple" size="md" w="42rem">
-            <Thead>
-              <Tr>
-                <Th color={fgDarker} fontSize="sm">
-                  Student Number
-                </Th>
-                <Th color={fgDarker} isNumeric fontSize="sm">
-                  Action
-                </Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {FAKE_DATA.map((data, index) => (
-                <Tr key={index}>
-                  <Td>{data.studentNumber}</Td>
-                  <Td isNumeric>
-                    <HStack gap="2" justify="end">
-                      <Button colorScheme="blue" size="sm">
-                        Download JSON
-                      </Button>
-                      <Button colorScheme="green" size="sm">
-                        Download CSV
-                      </Button>
-                    </HStack>
-                  </Td>
-                </Tr>
-              ))}
-            </Tbody>
-          </Table>
-        </Box>
+        <Accordion
+          allowMultiple
+          width={"container.md"}
+          backgroundColor="white"
+          rounded="lg"
+        >
+          {Object.entries(files).map(([studentNumber, data], index) => (
+            <AccordionItem key={index}>
+              <AccordionButton
+                _hover={{ backgroundColor: "white" }}
+                rounded="lg"
+              >
+                <Box flex="1" textAlign="left" my="2">
+                  <ChakraText fontWeight="bold" fontSize="lg">
+                    {studentNumber}
+                  </ChakraText>
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+              <AccordionPanel pb={4}>
+                {data.map((entry, index) => {
+                  const url =
+                    entry.jsonFileUrl !== ""
+                      ? entry.jsonFileUrl
+                      : entry.csvFileUrl;
+                  const entryName = parseFileName(url);
+                  return (
+                    <Flex
+                      key={index}
+                      justify="space-between"
+                      py="4"
+                      borderTopWidth={1}
+                      borderTopColor={bg}
+                    >
+                      <span>{entryName}</span>
+                      <Flex>
+                        {entry.jsonFileUrl !== "" ? (
+                          <Button
+                            as="a"
+                            colorScheme="blue"
+                            href={entry.jsonFileUrl}
+                            target="_blank"
+                            size="sm"
+                          >
+                            Download JSON
+                          </Button>
+                        ) : null}
+                        {entry.csvFileUrl !== "" ? (
+                          <Button
+                            as="a"
+                            colorScheme="green"
+                            href={entry.csvFileUrl}
+                            target="_blank"
+                            size="sm"
+                          >
+                            Download CSV
+                          </Button>
+                        ) : null}
+                      </Flex>
+                    </Flex>
+                  );
+                })}
+              </AccordionPanel>
+            </AccordionItem>
+          ))}
+        </Accordion>
       </Box>
     </Flex>
   );
