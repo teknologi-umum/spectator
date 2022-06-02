870dc5773b0cc69a112280515bf73730a56e43b7
elianiva <dicha.arkana03@gmail.com>
2022-06-02 07:33:41 +0700

    fix(backend): report first question runtime stdout

--- backend/Spectator.Piston/PistonClient.cs
+++ backend/Spectator.Piston/PistonClient.cs
@@ -120,12 +120,12 @@ namespace Spectator.Piston {
 				);
 			}
 
-			if (!IsTwinkleTwinkleLittleStarLyrics(executeResult.Compile.Stdout)) {
+			if (!IsTwinkleTwinkleLittleStarLyrics(executeResult.Runtime.Stdout)) {
 				return ImmutableArray.Create<TestResultBase>(
 					new FailingTestResult(
 						TestNumber: 1,
 						ExpectedStdout: "Twinkle twinkle little star\nHow I wonder what you are\nUp above the world so high\nLike a diamond in the sky\nTwinkle twinkle little star\nHow I wonder what you are",
-						ActualStdout: executeResult.Compile.Stdout
+						ActualStdout: executeResult.Runtime.Stdout
 					)
 				);
 			}
