21459b8bd4b8ab97e3cc073f4884cb04b555c1f2
elianiva <dicha.arkana03@gmail.com>
2022-07-06 07:49:49 +0700

    refactor(backend): replace IEnumerable with IReadOnlyList
    Co-authored-by: Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>

--- backend/Spectator.DomainModels/ExamReportDomain/Report.cs
+++ backend/Spectator.DomainModels/ExamReportDomain/Report.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace Spectator.DomainModels.ExamReportDomain {
 	public class Report {
-		public IEnumerable<ReportFile> ReportFiles { get; private init; }
+		public IReadOnlyList<ReportFile> ReportFiles { get; private init; }
 
 		public Report(IEnumerable<ReportFile> reportFiles, string videoUrl) {
 			ReportFiles = reportFiles;
