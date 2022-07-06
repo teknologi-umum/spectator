395ee5980b49b1d36896eab388719ca2e827c7d0
elianiva <dicha.arkana03@gmail.com>
2022-07-06 07:50:26 +0700

    refactor(backend): adjust constructor to accept IReadOnlyList
    Co-authored-by: Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>

--- backend/Spectator.DomainModels/ExamReportDomain/Report.cs
+++ backend/Spectator.DomainModels/ExamReportDomain/Report.cs
@@ -4,7 +4,7 @@ namespace Spectator.DomainModels.ExamReportDomain {
 	public class Report {
 		public IReadOnlyList<ReportFile> ReportFiles { get; private init; }
 
-		public Report(IEnumerable<ReportFile> reportFiles, string videoUrl) {
+		public Report(IReadOnlyList<ReportFile> reportFiles, string videoUrl) {
 			ReportFiles = reportFiles;
 		}
 	}
