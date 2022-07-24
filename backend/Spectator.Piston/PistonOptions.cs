f0096d2d3ea93ae07c274ad80712f65dd9060235
Reinaldy Rafli <aldy505@proton.me>
2022-07-24 09:24:18 +0700

    feat: java has its own run and compile timeout

--- backend/Spectator.Piston/PistonOptions.cs
+++ backend/Spectator.Piston/PistonOptions.cs
@@ -2,8 +2,8 @@
 	public class PistonOptions {
 		public string Address { get; set; } = "http://localhost:50051";
 		public int MaxConcurrentExecutions { get; set; } = 2;
-		public int CompileTimeout { get; set; } = 5000;
-		public int RunTimeout { get; set; } = 3000;
+		public int CompileTimeout { get; set; } = 7500;
+		public int RunTimeout { get; set; } = 5000;
 		public int MemoryLimit { get; set; } = 200_000_000;
 	}
 }
