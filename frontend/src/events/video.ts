923252920b91153ac710b0f2bc25f5dbf1738ed8
elianiva <dicha.arkana03@gmail.com>
2022-07-31 20:52:19 +0700

    fix(frontend): correctly send video chunk

--- frontend/src/events/video.ts
+++ frontend/src/events/video.ts
@@ -1,16 +1,16 @@
 import type { UploadRequest } from "@/events/video.worker";
 
 const worker = new Worker(new URL("./video.worker.ts", import.meta.url), {
-  type: "module"
+  type: "module",
 });
 
 export function uploadVideo(accessToken: string, startedAt: number) {
   return (e: BlobEvent) => {
     const data: UploadRequest = {
       blob: e.data,
-      size: e.data.size.toString(),
       accessToken: accessToken,
-      startedAt: startedAt
+      startedAt: startedAt,
+      stoppedAt: Math.round(e.timeStamp),
     };
     worker.postMessage(data);
   };
