90fdd96a6ed71cb10e68346ead9a322ec22bcd62
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2022-10-10 17:17:50 +0700

    Increase MaxConcurrentExecution from 2 to 100

--- backend/Spectator.Piston/PistonOptions.cs
+++ backend/Spectator.Piston/PistonOptions.cs
@@ -1,7 +1,7 @@
 ﻿namespace Spectator.Piston {
 	public class PistonOptions {
 		public string Address { get; set; } = "http://localhost:50051";
-		public int MaxConcurrentExecutions { get; set; } = 2;
+		public int MaxConcurrentExecutions { get; set; } = 100;
 		public int CompileTimeout { get; set; } = 7500;
 		public int RunTimeout { get; set; } = 5000;
 		public int MemoryLimit { get; set; } = 200_000_000;
